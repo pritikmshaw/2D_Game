a452a01b9aefaef0b3a6e5c971b896b7d035c7cb
pritikmshaw <pritikmshaw18@gmail.com>
2020-07-17 00:12:17 +0530

    updating

---
+++ BirdShooter/app/src/main/java/com/example/birdshooter/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.example.birdshooter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

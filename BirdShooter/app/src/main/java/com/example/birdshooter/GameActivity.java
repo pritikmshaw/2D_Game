c34b418857c7dfe5da4863ce139b0d9c78d0d4b3
pritikmshaw <pritikmshaw18@gmail.com>
2020-07-17 16:23:50 +0530

    images

---
+++ BirdShooter/app/src/main/java/com/example/birdshooter/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.birdshooter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ BirdShooter/app/src/main/java/com/example/birdshooter/GameView.java
@@ -0,0 +1,78 @@
+package com.example.birdshooter;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable{
+
+    private Thread thread;
+    private boolean isPlaying;
+    private Background background1, background2;
+
+
+
+    public GameView(Context context, int screenX, int screenY) {
+        super(context);
+
+        background1 = new Background(screenX, screenY, getResources());
+        background2 = new Background(screenX, screenY, getResources());
+
+        background2.x = screenX;
+    }
+
+    @Override
+    public void run() {
+
+        while (isPlaying) {
+
+            update ();
+            draw();
+            sleep ();
+        }
+    }
+
+    private void update() {
+
+        background1.x -= 10;
+        background2.x -= 10;
+
+        if(background1.x + background1.background.getWidth() < 0) {
+            
+        }
+
+    }
+
+    private void draw () {
+
+    }
+
+    private void sleep () {
+
+        try {
+            thread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void resume() {
+
+        isPlaying=true;
+        thread = new Thread(this);
+        thread.start();
+
+    }
+
+    public void pause() {
+
+        try {
+            isPlaying = false;
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}

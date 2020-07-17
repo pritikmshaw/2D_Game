a0cd739926b360fa61e10db9649040f4f13d468d
pritikmshaw <pritikmshaw18@gmail.com>
2020-07-18 01:16:53 +0530

    game view created and game activity modified

--- BirdShooter/app/src/main/java/com/example/birdshooter/GameActivity.java
+++ BirdShooter/app/src/main/java/com/example/birdshooter/GameActivity.java
@@ -2,12 +2,34 @@ package com.example.birdshooter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Point point =new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        gameView= new GameView(this, point.x, point.y);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause(); // to pause the game
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume(); // to resume the game
+
     }
 }
--- BirdShooter/app/src/main/java/com/example/birdshooter/GameView.java
+++ BirdShooter/app/src/main/java/com/example/birdshooter/GameView.java
@@ -1,12 +1,17 @@
 package com.example.birdshooter;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable{
 
     private Thread thread;
     private boolean isPlaying;
+    private int screenX, screenY;
+    private Paint paint;
+    private float screenRatioX, screenRatioY;
     private Background background1, background2;
 
 
@@ -14,10 +19,18 @@ public class GameView extends SurfaceView implements Runnable{
     public GameView(Context context, int screenX, int screenY) {
         super(context);
 
+        this.screenX = screenX ;
+        this.screenY = screenY ;
+
+        screenRatioX = 1920f/screenX;
+        screenRatioY = 1080f/screenY;
+
         background1 = new Background(screenX, screenY, getResources());
         background2 = new Background(screenX, screenY, getResources());
 
         background2.x = screenX;
+
+        paint = new Paint();
     }
 
     @Override
@@ -33,16 +46,29 @@ public class GameView extends SurfaceView implements Runnable{
 
     private void update() {
 
-        background1.x -= 10;
-        background2.x -= 10;
+        background1.x -= 10 * screenRatioX;
+        background2.x -= 10 * screenRatioX;
 
         if(background1.x + background1.background.getWidth() < 0) {
+            background1.x = screenX;
+        }
 
+        if(background2.x + background2.background.getWidth() < 0) {
+            background2.x = screenY;
         }
 
     }
 
     private void draw () {
+        if(getHolder().getSurface().isValid()) {
+            Canvas canvas = getHolder().lockCanvas();
+            canvas.drawBitmap(background1.background, background1.x ,background1.y , paint);
+            canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
+
+            getHolder().unlockCanvasAndPost(canvas); // to show the drawing on the screen
+
+        }
+
 
     }
 
